e2a4b48734421fa37a073c97133c925e53543c1a
Seumas Gordon <SJGordon1@student.fullsail.edu>
2020-03-19 14:19:33 -0400

    nav bar is done now needing login

---
+++ app/src/main/java/com/example/bottomnamviagtionbar/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.bottomnamviagtionbar;
+
+
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = findViewById(R.id.navigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Home");
+        loadFragment(new HomeFragment());
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    toolbar.setTitle("Home");
+                    loadFragment(new HomeFragment());
+                    return true;
+                case R.id.account:
+                    toolbar.setTitle("Account");
+                    loadFragment(new AccountFragment());
+                    return true;
+                case R.id.budget:
+                    toolbar.setTitle("Budget");
+                    loadFragment(new BudgetFragment());
+                    return true;
+                case R.id.history:
+                    toolbar.setTitle("History");
+                    loadFragment(new HistoryFragment());
+                    return true;
+                case R.id.paybills:
+                    toolbar.setTitle("PayBills");
+                    loadFragment(new PaybillsFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
