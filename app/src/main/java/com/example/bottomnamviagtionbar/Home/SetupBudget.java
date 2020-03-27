92366283ca1ac2dac31a04776b90f86c214447fc
Seumas Gordon <SJGordon1@student.fullsail.edu>
2020-03-26 22:36:18 -0400

    calender no longer covers logout button

--- app/src/main/java/com/example/bottomnamviagtionbar/Home/SetupBudget.java
+++ app/src/main/java/com/example/bottomnamviagtionbar/Home/SetupBudget.java
@@ -1,10 +1,13 @@
-package com.example.bottomnamviagtionbar;
+package com.example.bottomnamviagtionbar.Home;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.bottomnamviagtionbar.BudgetVar;
+import com.example.bottomnamviagtionbar.R;
 
 
 public class SetupBudget extends AppCompatActivity {
